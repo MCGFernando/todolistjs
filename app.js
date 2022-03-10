b03977ba57f8b1851f58b57ad32acd1fbb26f98d
MCGFernando <marofernando1991@gmail.com>
2022-03-10 22:29:52 +0000

    ToDo List

---
+++ app.js
@@ -0,0 +1,64 @@
+const addForm  = document.querySelector('.add')
+const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
+
+const generateTemplate = todo =>{
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>`
+      list.innerHTML += html
+}
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const todo = addForm.add.value.trim()
+    if (todo.length){
+        generateTemplate(todo)
+        addForm.reset()
+    }
+    
+})
+
+list.addEventListener('click', e => {
+    const trash = e.target.classList.length
+    /* if (trash){
+        console.log('Trash')
+    }else{
+        console.log('span')
+    } */
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove()
+    }
+})
+
+const filterTodos = (term) =>{
+    Array.from(list.children)
+        .filter((todo)=> !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo)=> todo.classList.add('filtered'))
+
+    Array.from(list.children)
+        .filter((todo)=> todo.textContent.toLowerCase().includes(term))
+        .forEach((todo)=> todo.classList.remove('filtered'))
+}
+
+
+search.addEventListener('keyup', () =>{
+    const term = search.value.trim().toLowerCase()
+
+    /* const myArray = list.querySelectorAll('span')
+
+    console.log(myArray)
+
+    myArray.forEach((el)=>{
+        let ver
+        if (el.innerText.contains(text)){
+            ver = true
+        }else{
+            ver = false
+        }
+        console.log(el.innerText, ver) */
+        filterTodos(term)
+    
+})
